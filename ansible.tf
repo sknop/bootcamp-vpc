9adb028cffbb92d3de60b6321d501324dd990ad6
Sven Erik Knop <seknop@gmail.com>
2024-10-24 17:10:47 +0000

    Trigger Ansible if samba or jumphost have changed

--- ansible.tf
+++ ansible.tf
@@ -2,6 +2,12 @@ resource "null_resource" "ansible-playbook" {
   provisioner "local-exec" {
     command = "ansible-playbook playbook.yml"
   }
+
+  triggers = {
+    sambahost_id = aws_instance.sambahost.id
+    jumphost_id = aws_instance.jumphost.id
+  }
+
   depends_on = [
     aws_instance.jumphost,
     aws_instance.sambahost,
