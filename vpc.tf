11a152245ba060663b814ecc669355d3185394de
Sven Erik Knop <seknop@gmail.com>
2026-03-05 19:59:18 +0000

    Superfluous len removed, mandatory closing bracket added

--- vpc.tf
+++ vpc.tf
@@ -16,8 +16,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  len      = length(data.aws_availability_zones.available.names)
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     cflt_environment  = var.cflt_environment
